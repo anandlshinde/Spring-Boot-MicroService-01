be1d4ab5b40050fb9d484ee79e19560c6e01928d
anandlshinde <anandshinde.22@gmail.com>
2023-01-03 22:22:59 +0530

    added changes config server and client

--- stock-service-config/stock-service-config/src/main/java/com/devhelper/stockserviceconfig/StockServiceConfigApplication.java
+++ stock-service-config/stock-service-config/src/main/java/com/devhelper/stockserviceconfig/StockServiceConfigApplication.java
@@ -2,8 +2,12 @@ package com.devhelper.stockserviceconfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
 public class StockServiceConfigApplication {
 
 	public static void main(String[] args) {
