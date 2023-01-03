6acee62e4ab61d8c967939fa1d0a34d788bd2ed4
anandlshinde <anandshinde.22@gmail.com>
2023-01-03 19:45:29 +0530

    added config server

---
+++ stock-service-config/stock-service-config/src/main/java/com/devhelper/stockserviceconfig/StockServiceConfigApplication.java
@@ -0,0 +1,13 @@
+package com.devhelper.stockserviceconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockServiceConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockServiceConfigApplication.class, args);
+	}
+
+}
---
+++ stock-service-config/stock-service-config/src/test/java/com/devhelper/stockserviceconfig/StockServiceConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devhelper.stockserviceconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockServiceConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
